7b0e0dbd63a99e5220fc282c8c5d2de9dfae1d87
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-02-25 14:46:45 +0200

    initial setup

--- Questionnaire/ViewController.swift
+++ Questionnaire/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionText: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionText.text = "Hello World!"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        print(sender.titleLabel!.text!)
+    }
+    
 }
 
